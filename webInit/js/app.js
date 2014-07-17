a56442ed92d96adc49a5717a68865777930f1538
Jungtae Kim <jt2kim@uwaterloo.ca>
2014-07-16 20:28:28 -0700

    Initial version of framework, refactoring to common component needed

--- webInit/js/app.js
+++ webInit/js/app.js
@@ -10,6 +10,22 @@ define([
     Backbone,
     Router){
   var initialize = function(){
+
+    Handlebars.registerHelper('getTemplateAjax', function(path, callback) {
+        var source,
+            template;
+
+        $.ajax({
+            url: 'templates/' + path,
+                success: function(data) {
+                    source    = data;
+                    template  = Handlebars.compile(source);
+
+                    //execute the callback if passed
+                    if (callback) callback(template);
+            }
+        });
+    });
     // Pass in our Router module and call it's initialize function
     Router.initialize();
   };
